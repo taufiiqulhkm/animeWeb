3c487e3dd933807142e1655eb7988c47f760f991
taufiiq <taufiiqulhakim@gmailcom>
2023-11-20 11:52:58 +0700

    sesi 6

--- src/app/search/[keyword]/page.jsx
+++ src/app/search/[keyword]/page.jsx
@@ -1,4 +1,5 @@
 
+import { getAnimeResponse } from "@/app/libs/api-libs"
 import AnimeList from "@/components/AnimeList"
 import Header from "@/components/AnimeList/Header"
 
@@ -7,11 +8,10 @@ const Page = async ({ params }) => {
     
     const decodeKeyword = decodeURI(keyword)
     
-    const response = await fetch
-    (`${process.env.NEXT_PUBLIC_API_BASE_URL}/anime?q=${decodeKeyword}`)
-    const searchAnime = await response.json()
-
-
+    // const response = await fetch
+    // (`${process.env.NEXT_PUBLIC_API_BASE_URL}/anime?q=${decodeKeyword}`)
+    // const searchAnime = await response.json()
+    const searchAnime = await getAnimeResponse("anime", `q=${decodeKeyword}`)
     return (
         <>
             <section>
---
+++ src/components/Utilities/VideoPlayer.jsx
@@ -0,0 +1,52 @@
+"use client";
+
+import { useState } from "react";
+import YouTube from "react-youtube";
+
+const VideoPlayer = (youtubeId) => {
+  const [isOpen, setIsOpen] = useState(true);
+
+  const handleVideoPlayer = () => {
+    setIsOpen((prevState) => !prevState);
+  };
+
+  const option = {
+    width: "300",
+    height: "250",
+  };
+
+  const Player = () => {
+    return (
+      <div className="fixed bottom-2 right-2">
+        <button
+          onClick={handleVideoPlayer}
+          className="text-color-primary float-right px-3 nb-1"
+        >
+          X
+        </button>
+        <YouTube
+          videoId={youtubeId}
+          onReady={(event) => event.target.pauseVideo()}
+          opts={option}
+          onError={() => alert("video is broken, please try another video")}
+        />
+      </div>
+    );
+  };
+
+  const ButtonOpenPlayer = () => {
+    return (
+      <button
+        onClick={handleVideoPlayer}
+        className="rounded fixed bottom-5 right-5 w-32 bg-color-primary text-color-dark text-xl hover:bg-color-accent
+        transition-all shadow-xl"
+      >
+        Tonton Trailer
+      </button>
+    );
+  };
+
+  return isOpen ? <Player /> : <ButtonOpenPlayer />;
+};
+
+export default VideoPlayer;
